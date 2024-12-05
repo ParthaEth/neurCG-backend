cf33a4e80296ada023756d5216ce43bd7c0b6377
pghosh <parthaghosh.iitbbs@gmail.com>
2024-12-05 12:36:05 +0100

    adding routed for flask aabckend  to replace s3

--- src/routes/getFlaskFile.ts
+++ src/routes/getFlaskFile.ts
@@ -5,30 +5,32 @@ const router = Router();
 configDotenv()
 
 router.post("/", async (req: Request, res: Response) => {
-    const { subpath } = req.params
+    const { subpath } = req.body
     const flaskUrl = process.env.FLASK_BACKEND_ML_URL as string
-    try {
-        const formData = new FormData()
-        formData.append("subpath", subpath)
+//     try {
+//         const formData = new FormData()
+//         formData.append("subpath", subpath)
+//
+//         const response = await axios.post(`${flaskUrl}/get-file`, formData, {
+//             timeout: 600000,
+//             responseType: 'arraybuffer',
+//             headers: {
+//                 'Content-Type': 'multipart/form-data',
+//             }
+//         })
+//         if (!response.data || !(response.data.length > 0)) {
+//             throw new Error('Empty or invalid file response from Flask API');
+//         }
+//         const fileBuffer = Buffer.from(response.data)
+//         res.set('Content-Type', 'application/octet-stream')
+//         res.send(fileBuffer)
+//
+//     } catch (error) {
+//         console.log(error)
+//         return res.status(500).json({ success: false, message: "Internal Server Error" })
+//     }
 
-        const response = await axios.post(`${flaskUrl}/get-file`, formData, {
-            timeout: 600000,
-            responseType: 'arraybuffer',
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            }
-        })
-        if (!response.data || !(response.data.length > 0)) {
-            throw new Error('Empty or invalid file response from Flask API');
-        }
-        const fileBuffer = Buffer.from(response.data)
-        res.set('Content-Type', 'application/octet-stream')
-        res.send(fileBuffer)
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ success: false, message: "Internal Server Error" })
-    }
+return res.send(flaskUrl);
 })
 
 export { router }
